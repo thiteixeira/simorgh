f731b88048a2445ffb1c14c4e0481003b03dd41f
Andrew Bennett <andrew.bennett07@bbc.co.uk>
2021-01-29 17:14:04 +0000

    Update serviceSchemaUpdate.test.js

--- src/app/lib/config/serviceSchemaUpdate.test.js
+++ src/app/lib/config/serviceSchemaUpdate.test.js
@@ -1,6 +1,11 @@
+var myArgs = process.argv.slice(2);
+
 const fs = require('fs');
 const serviceDir = '../../../../src/app/lib/config/services';
-const { service } = require(serviceDir + '/news.js');
+const [_, serviceFileName] = myArgs;
+const servicesPath = 'src/app/lib/config/services';
+const { service } = require(serviceDir + '/' + serviceFileName);
+const clipboardy = require('clipboardy');
 
 it('stuff', () => {
   const serviceFiles = fs.readdirSync('src/app/lib/config/services');
@@ -13,35 +18,35 @@ it('stuff', () => {
 
   consentBanner.cookie = {
     amp: {
-      accept: content['7'],
-      reject: content['9'],
+      accept: content['8'],
+      reject: content['10'],
       initial: {
         title: content['4'],
         description: consentBanner.cookie.description.international,
-        manage: 'Manage settings',
+        manage: content['9'],
       },
       manage: {
-        title: content['10'],
+        title: content['11'],
         description: {
-          para1: content['11'],
-          para2: content['12'],
-          heading2: content['13'],
-          para3: content['14'],
+          para1: content['12'],
+          para2: content['13'],
+          heading2: content['14'],
+          para3: content['15'],
           para4: {
-            text: content['15'],
+            text: content['16'],
             url:
               'https://www.bbc.co.uk/usingthebbc/strictly-necessary-cookies/',
           },
-          para5: content['16'],
-          heading3: content['17'],
-          para6: content['18'],
+          para5: content['17'],
+          heading3: content['18'],
+          para6: content['19'],
           para7: {
-            text: content['19'],
+            text: content['20'],
             url:
               'https://www.bbc.com/usingthebbc/cookies/how-does-the-bbc-use-cookies-for-advertising/',
           },
-          para8: content['20'],
-          para9: content['21'],
+          para8: content['21'],
+          para9: content['22'],
         },
       },
     },
@@ -57,33 +62,23 @@ it('stuff', () => {
 
   console.log('\n\n\n');
 
-  console.log(JSON.stringify(consentBanner.cookie, null, 2));
+  const newCookie = JSON.stringify(consentBanner.cookie, null, 2);
+
+  const filePath = `${servicesPath}/${serviceFileName}`;
+  const contents = fs.readFileSync(filePath, 'utf8');
+  const output = contents.replace(
+    /cookie: {(.|\n)*media:/g,
+    `cookie: ${newCookie}\},media:`,
+  );
+  fs.writeFileSync(filePath, output);
 });
 
 const parseSheetContent = () => {
-  const source = `Let us know you agree to data collection on AMP
-  On this accelerated mobile page (AMP) we and our partners collect browsing data to provide you with the best online experience and to personalise the advertising shown to you.
-  "Some of these technologies are essential to the running of this page, while others such as those for personalised advertising are optional and require your consent.
-  You may be asked to set these preferences again if you visit non-AMP bbc.com pages.  "
-  Accept data collection and continue
-  Manage my settings
-  Reject data collection and continue
-  Manage consent settings on AMP pages 
-  These settings apply to AMP pages only. You may be asked to set these preferences again when you visit non-AMP BBC pages.
-  The lightweight mobile page you have visited has been built using Google AMP technology. 
-  Strictly necessary data collection
-  To make our web pages work, we store some limited information on your device without your consent.
-  Read more about the essential information we store on your device to make our web pages work.
-  We use local storage to store your consent preferences on your device.
-  Optional data collection
-  When you consent to data collection on AMP pages you are consenting to allow us to display personalised ads that are relevant to you when you are outside of the UK.
-  Read more about how we personalise ads in the BBC and our advertising partners.
-  "You can choose not to receive personalised ads by clicking “Reject data collection and continue” below. Please note that you will still see advertising, but it will not be personalised to you.
-   "
-  You can change these settings by clicking “Ad Choices / Do not sell my info” in the footer at any time. `.replace(
-    /\"/g,
-    '',
-  );
+  const source = clipboardy
+    .readSync()
+    .replace(/"\n/g, '\n')
+    .replace(/\n\s*"/g, '\n')
+    .replace(/""/g, '"');
 
   console.log(source);
 
@@ -92,19 +87,21 @@ const parseSheetContent = () => {
     return Object.assign({}, ...first);
   };
 
-  const joinKey = [
-    ['6', '7'],
-    ['21', '22'],
-  ];
+  // const joinKey = [[22, 23]];
+  const joinKey = [];
 
   let processed = buildKey(source.split('\n').map(x => x.trim()));
 
+  console.log(JSON.stringify(processed, null, 2));
+
   joinKey.forEach(([dest, source]) => {
-    processed[dest] = `${processed[dest]}\n${processed[source]}`;
+    processed[dest] = `${processed[dest]}\n${processed[source]}`.trim();
     delete processed[source];
   });
 
   const result = buildKey(Object.values(processed));
+
+  console.log(JSON.stringify(result, null, 2));
 
   return result;
 };
